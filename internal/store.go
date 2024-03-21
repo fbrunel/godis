3cc6e27dd68969351fa37af6d424d8022945f5f4
Fred Brunel <fbrunel@gmail.com>
2024-03-21 17:50:26 -0400

    Changes the visibility of the Store fields

--- internal/store.go
+++ internal/store.go
@@ -5,30 +5,30 @@ import (
 )
 
 type Store struct {
-	HMap map[string]string
-	Mux  sync.RWMutex
+	data map[string]string
+	mux  sync.RWMutex
 }
 
 func NewStore() *Store {
 	return &Store{
-		HMap: make(map[string]string),
+		data: make(map[string]string),
 	}
 }
 
 func (st *Store) Set(key string, value string) {
-	st.Mux.Lock()
-	defer st.Mux.Unlock()
-	st.HMap[key] = value
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	st.data[key] = value
 }
 
 func (st *Store) Get(key string) string {
-	st.Mux.RLock()
-	defer st.Mux.RUnlock()
-	return st.HMap[key]
+	st.mux.RLock()
+	defer st.mux.RUnlock()
+	return st.data[key]
 }
 
 func (st *Store) Del(key string) {
-	st.Mux.Lock()
-	defer st.Mux.Unlock()
-	delete(st.HMap, key)
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	delete(st.data, key)
 }
